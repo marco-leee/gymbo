c7708c71399a827f2ccc58fdfe7b97e313a2cd27
marco-leee <marco.leee@outlook.com>
2025-06-12 20:01:18 +0800

    new: errors

---
+++ services/shared/global_error/functions.go
@@ -0,0 +1,17 @@
+package global_error
+
+import (
+	"connectrpc.com/connect"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+)
+
+func AddDetails(err *connect.Error, domain string, reason string) {
+	info := &errdetails.ErrorInfo{
+		Reason: reason,
+		Domain: domain,
+	}
+
+	if detail, detailErr := connect.NewErrorDetail(info); detailErr == nil {
+		err.AddDetail(detail)
+	}
+}
