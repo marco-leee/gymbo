e30a9671c5cfe9731ee35306875b9d081db0eb62
marco-leee <marco.leee@outlook.com>
2025-06-12 20:11:22 +0800

    update: add org field, utils str, db conf env

--- services/shared/env/init.go
+++ services/shared/env/init.go
@@ -1,9 +1,11 @@
 package env
 
 var (
-	Env Environment
+	Env          Environment
+	DBConnString string
 )
 
 func init() {
 	Env = NewEnvironment(GetEnv("ENV", "production"))
+	DBConnString = GetEnv("DATABASE_URL", "host=localhost user=admin password=local dbname=gymbo port=32800 sslmode=disable TimeZone=Asia/Hong_Kong")
 }
