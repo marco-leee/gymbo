e14bff4709b6631c2d14acb8eddc34aa0a2e7fd3
marco-leee <marco.leee@outlook.com>
2025-06-18 08:38:25 +0800

    update: trainer user utils

--- services/shared/models/trainer.go
+++ services/shared/models/trainer.go
@@ -3,6 +3,9 @@ package models
 import (
 	"database/sql"
 	"time"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+	v1_entities "gymbo.stixman.co/shared/gen/entities/v1"
 )
 
 type Trainer struct {
@@ -12,4 +15,17 @@ type Trainer struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt sql.NullTime
+}
+
+func (t *Trainer) ToProto() *v1_entities.Trainer {
+	return &v1_entities.Trainer{
+		Id:        t.ID,
+		FullName:  t.User.FullName,
+		FirstName: &t.User.FirstName,
+		LastName:  &t.User.LastName,
+		Email:     t.User.Email,
+		CreatedAt: timestamppb.New(t.CreatedAt),
+		UpdatedAt: timestamppb.New(t.UpdatedAt),
+		DeletedAt: timestamppb.New(t.DeletedAt.Time),
+	}
 }
