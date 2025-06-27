2fc765aea63a7a49595ca59c17d79faa330158eb
marco-leee <marco.leee@outlook.com>
2025-06-27 09:45:24 +0800

    update: use path in s3

--- backend/src/storage/S3.py
+++ backend/src/storage/S3.py
@@ -1,13 +1,13 @@
 from .base import StorageProvider
 import boto3
+from pathlib import Path
 
 
 class S3StorageProvider(StorageProvider):
     def __init__(
         self, bucket: str, access_key: str, secret_key: str, endpoint_url: str = None
     ) -> None:
-        super().__init__()
-        self.bucket = bucket
+        super().__init__(bucket)
         self.client = boto3.client(
             "s3",
             aws_access_key_id=access_key,
@@ -15,14 +15,14 @@ class S3StorageProvider(StorageProvider):
             endpoint_url=endpoint_url,
         )
 
-    def download_object(self, object_key: str, destination_path: str) -> str:
+    def download_object(self, object_key: str, destination_path: Path) -> None:
         # TODO: Add download file function
         try:
             self.client.download_file(self.bucket, object_key, destination_path)
         except Exception as e:
             raise e
 
-    def upload_object(self, object_path: str, object_key: str) -> None:
+    def upload_object(self, object_path: Path, object_key: str) -> None:
         # TODO: Add upload file function
         try:
             self.client.upload_file(object_path, self.bucket, object_key)
