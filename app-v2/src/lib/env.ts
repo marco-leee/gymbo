bf0f5569a14be29690233ca5766ffa799fb63a77
marco-leee <marco.leee@outlook.com>
2026-03-14 08:51:26 +0800

    update: set bucket to auto

--- app-v2/src/lib/env.ts
+++ app-v2/src/lib/env.ts
@@ -3,7 +3,7 @@ import { env as privateEnv } from '$env/dynamic/private';
 export const env = {
 	MONGO_URI: privateEnv.MONGO_URI || 'mongodb://admin:local@localhost:27017/gymbo?authSource=admin',
 	STORAGE_ENDPOINT: privateEnv.STORAGE_ENDPOINT || 'http://localhost:9000',
-	STORAGE_REGION: privateEnv.STORAGE_REGION || 'ap-southeast-1',
+	STORAGE_REGION: privateEnv.STORAGE_REGION || 'auto',
 	STORAGE_ACCESS_KEY: privateEnv.STORAGE_ACCESS_KEY || 'admin',
 	STORAGE_SECRET_KEY: privateEnv.STORAGE_SECRET_KEY || 'qwertyui',
 	STORAGE_BUCKET: privateEnv.STORAGE_BUCKET || 'gymbo',
